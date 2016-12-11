e83c5988e996a2909bed1cac009704b71060aa3d
András Ecker <andras.ecker@epfl.ch>
2016-12-11 23:15:03 +0100

    everything made consistent

--- scripts/build_vocab.sh
+++ scripts/build_vocab.sh
@@ -2,4 +2,4 @@
 
 # Note that this script uses GNU-style sed. On Mac OS, you are required to first
 #    brew install gnu-sed --with-default-names
-cat ../data/pos_train.txt ../data/neg_train.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ../data/vocab.txt
+cat ../data/train_pos.txt ../data/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ../data/vocab.txt
